c6a4116e185db49a49b4d0d41835c6a36d39d3b6
Muharrem Ustaoglu <muharremustaoglu53@gmail.com>
2024-05-14 21:02:11 +0300

    render setting is added

--- Commune/Commune/settings.py
+++ Commune/Commune/settings.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -10,12 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q_ej**zz+cal8m$&l&)c)p4!7^ev*m+hn2yvm=x+o03#254v26'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG","False").lower() =="true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOST").split(" ")
 
 
 # Application definition
@@ -84,6 +85,10 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+database_url =os.environ.get("DATABASE_URL")
+DATABASES["default"]=dj_database_url.parse(database_url)
+
+
 
 
 # Password validation
